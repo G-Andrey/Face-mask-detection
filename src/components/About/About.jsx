718995caa3060a91e9115a3493e5ca01b86bbfa5
unknown <g.andrey77@gmail.com>
2022-06-17 23:03:01 -0400

    updated about model

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,20 +15,20 @@ const About = () => {
           Each prediction is displayed with it's corresponding confidence threshold [25%-100%]  
         </li>
         <li>
-          The model was trained on the YoloV5 architecture using transfer learning from the YoloV5s model. 
+          Trained on the YoloV5 architecture using transfer learning from the YoloV5s model. 
           The YoloV5s model was chosen as it strikes a good balance between size and predictive precision
         </li>
         <li>
-          This computer vision model was trained to detect and classify whether a human is wearing a mask or not with bounding box annotations
+          Trained via Google Colab using a P100 GPU, taking 3.5 hours
         </li>
         <li>
-        This computer vision model was trained to detect and classify whether a human is wearing a mask or not with bounding box annotations
+          Trained on over 2,700 images containing over 4,000 annoations
         </li>
         <li>
-        This computer vision model was trained to detect and classify whether a human is wearing a mask or not with bounding box annotations
+          Model was tested on 680 validation images that were withheld from the training set
         </li>
         <li>
-        This computer vision model was trained to detect and classify whether a human is wearing a mask or not with bounding box annotations
+          Achieved 88% precision and 89% recall when tested on the validation images
         </li>
       </ul>
       <img />
