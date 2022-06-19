2e03272a6681f5c61864437e2902c39d9bf50823
unknown <g.andrey77@gmail.com>
2022-06-18 21:39:43 -0400

    about page styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,10 @@ import './About.css'
 
 const About = () => {
   return (
+    <div style={{height:'100vh', backgroundColor:'#2d3748'}}>
+    <div className="hero-container">
+      <video src='/videos/video-2.mp4' autoPlay loop muted className='hero-video'/>
+    </div>
     <div className="about-container" >
       <div className="title">
         About the Model
@@ -15,14 +19,13 @@ const About = () => {
           Each prediction is displayed with it's corresponding confidence threshold [25%-100%]  
         </li>
         <li>
-          Trained on the YoloV5 architecture using transfer learning from the YoloV5s model. 
-          The YoloV5s model was chosen as it strikes a good balance between size and predictive precision
+          Trained on the YoloV5 architecture using transfer learning from the YoloV5s model
         </li>
         <li>
-          Trained via Google Colab using a P100 GPU, taking 3.5 hours
+          Trained via Google Colab using a P100 GPU taking 3.5 hours
         </li>
         <li>
-          Trained on over 2,700 images containing over 4,000 annoations
+          Trained on over 2,700 images containing over 3,000 annotations
         </li>
         <li>
           Model was tested on 680 validation images that were withheld from the training set
@@ -31,7 +34,14 @@ const About = () => {
           Achieved 88% precision and 89% recall when tested on the validation images
         </li>
       </ul>
-      <img />
+      <div className='about-btn-container'>
+        <a href="/model">
+          <button className="about-btn">
+            Test the model
+          </button>          
+        </a>
+      </div>
+    </div>
     </div>
   );
 };
